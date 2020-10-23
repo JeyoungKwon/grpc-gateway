c7e155f6903852893704e8203f58df9b7a8b3b41
Jeyoung Kwon <jkwon@clearstreet.io>
2020-10-23 14:51:53 -0400

    use gogo httpbody instead of google

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/antihax/optional v1.0.0
 	github.com/ghodss/yaml v1.0.0
+	github.com/gogo/googleapis v1.4.1-0.20201022092350-68b0159b7869
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
 	github.com/golang/protobuf v1.4.3
 	github.com/google/go-cmp v0.5.2
--- runtime/marshal_httpbodyproto.go
+++ runtime/marshal_httpbodyproto.go
@@ -1,8 +1,10 @@
 package runtime
 
 import (
-	"google.golang.org/genproto/googleapis/api/httpbody"
+	"github.com/gogo/googleapis/google/api"
 )
+
+//"google.golang.org/genproto/googleapis/api/httpbody"
 
 // HTTPBodyMarshaler is a Marshaler which supports marshaling of a
 // google.api.HttpBody message as the full response body if it is
@@ -16,7 +18,7 @@ type HTTPBodyMarshaler struct {
 // google.api.HttpBody message, otherwise it will fall back to the default Marshalers
 // content type.
 func (h *HTTPBodyMarshaler) ContentType(v interface{}) string {
-	if httpBody, ok := v.(*httpbody.HttpBody); ok {
+	if httpBody, ok := v.(*api.HttpBody); ok {
 		return httpBody.GetContentType()
 	}
 	return h.Marshaler.ContentType(v)
@@ -25,7 +27,7 @@ func (h *HTTPBodyMarshaler) ContentType(v interface{}) string {
 // Marshal marshals "v" by returning the body bytes if v is a
 // google.api.HttpBody message, otherwise it falls back to the default Marshaler.
 func (h *HTTPBodyMarshaler) Marshal(v interface{}) ([]byte, error) {
-	if httpBody, ok := v.(*httpbody.HttpBody); ok {
+	if httpBody, ok := v.(*api.HttpBody); ok {
 		return httpBody.Data, nil
 	}
 	return h.Marshaler.Marshal(v)
